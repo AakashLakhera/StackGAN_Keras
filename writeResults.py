c135a46205661d64c2dada277296cd88fef08fe6
AakashLakhera <47984348+AakashLakhera@users.noreply.github.com>
2019-11-16 23:29:39 +0530

    Add files via upload

--- writeResults.py
+++ writeResults.py
@@ -21,16 +21,15 @@ Nx = Ng+Nz
 Md = 4
 Mg = 16
 Nres = 4
-gen0_loc = 'Generator0_4.h5'
-gen_loc = 'Generator1.h5'
+gen0_loc = 'Generator0_4_2.h5'
+gen_loc = 'Generator1_2.h5'
 
 random.seed(time.time())
 np.random.seed(int(time.time()+0.5))
 
-_, Emb, captions = load_dataset('birds/test/', 'CUB_200_2011/', 32, True)
-print(Emb.shape, len(captions))
+_, Emb, captions = load_dataset('birds/test/', 'CUB_200_2011/', 32)
 
-gen0 = generator0(Nphi, Ng, Nz)
+gen0 = generator0_2(Nphi, Ng, Nz)
 try:
     gen0.load_weights(gen0_loc)
 except:
@@ -47,15 +46,18 @@ except:
 
 lenX = Emb.shape[0]
 r = random.randint(0, Emb.shape[1]-1)
-phi_t = Emb[:,r,:]
-tex = captions[:][r]
+phi_t = Emb[:lenX,r,:]
+if captions != []:
+    tex = captions[:][r]
 eps = np.random.normal(0, 1, [lenX, Ng])
 z = np.random.normal(0, 1, [lenX, Nz])
 x_img, _ = gen0.predict([phi_t, eps, z])
 eps = np.random.normal(0, 1, [lenX, Ng])
 x_test, _ = gen1.predict([phi_t, eps, x_img])
+x_test = 127.5*(x_test + 1)
 for k in range(x_test.shape[0]):
-    cv2.imwrite('ResultsII\\'+str(k)+'.jpg', x_test[k])
-    f = open('ResultsII\\'+str(k)+'.txt', 'w')
-    f.write(tex[k])
-    f.close()
+    cv2.imwrite('ResultsII_2\\'+str(k)+'.jpg', x_test[k])
+    if captions != []:
+        f = open('ResultsII_2\\'+str(k)+'.txt', 'w')
+        f.write(tex[k])
+        f.close()
